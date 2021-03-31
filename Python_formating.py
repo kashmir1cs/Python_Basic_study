b60703c3cddf7c669e2e22035479c950aecfa3cf
RASCALCS <63025037+kashmir1cs@users.noreply.github.com>
2021-03-31 21:13:41 +0900

    Python_formatting (how to use)

---
+++ Python_formating.py
@@ -0,0 +1,13 @@
+# format 사용 [], {}, ()
+# 중요함
+print()
+print('{} and {}'.format('You','Me'))
+print('{0} and {1} and {0}'.format('You','Me'))
+print ("{a} are {b}".format(a='You',b='Me'))
+
+print("%s's favorite number is %d" % ('Eunki',7)) #%s : 문자 %d : 정수 , %f: 실수 
+
+
+print("Test1 : %5d, price : %4.2f" % (776,6534.123))
+print("Test1 : {0: 5d}, Price : {1:4.2f}".format(776,6534.123))
+print("Test1: {a: 5d}, price: {b:4.2f}".format(a=776,b=6534.123)) 
